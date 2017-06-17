a5d0f29d848328c8d8d92f5dfe29565a43846daf
ethancwb <ethancwb@foxmail.com>
2017-06-17 11:54:17 -0400

    。

--- public/pokemon.controller.client.js
+++ public/pokemon.controller.client.js
@@ -5,9 +5,8 @@
 
     function PPController ($http, $location, $window) {
         var model = this
-
         PkSpr.process_dom();
-        // 
+        //
         // $.getJSON('http://pokeapi.co/api/v2/stat/1/', function(data) {
         //     console.log(data)
         // })
@@ -27,7 +26,7 @@
                             model.message = "Wrong username & password"
                         }
         })
-
+        
         model.disconnect = (function () {
             var url = '/database'
             return $http.get(url)
